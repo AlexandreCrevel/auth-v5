cb0b1387e872ebbca450e5c3c8bdb87289c90dbd
Alex <alexandre.crevel@outlook.com>
2024-02-15 13:07:40 +0100

    feat(auth): login with google OK

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -1,7 +1,23 @@
 import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
     debug: true,
     secret: process.env.NEXTAUTH_SECRET,
-providers: [],
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+      ],
+      callbacks: {
+        async signIn({
+            user, account, profile, email, credentials
+        }) {
+            return true
+        },
+        async redirect({ url, baseUrl}) {
+            return baseUrl + '/dashboard';
+        }
+      }
 };
