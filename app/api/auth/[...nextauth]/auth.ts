21311e7eee86fb874a3e5afa83e12fac3754f1bd
Alex <alex.crevel@gmail.com>
2024-02-23 11:00:42 +0100

    feat(prisma): working login with prisma

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -1,23 +1,26 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
+import prisma from '@/libs/prisma'
 
 export const authOptions: NextAuthOptions = {
     debug: true,
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         })
       ],
-      callbacks: {
-        async signIn({
-            user, account, profile, email, credentials
-        }) {
-            return true
-        },
-        async redirect({ url, baseUrl}) {
-            return baseUrl + '/dashboard';
-        }
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+      async signIn({
+          user, account, profile, email, credentials
+      }) {
+          return true
+      },
+      async redirect({ url, baseUrl}) {
+          return baseUrl + '/dashboard';
       }
+    }
 };
---
+++ libs/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+
+type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: undefined | prismaClientSingleton
+    };
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
